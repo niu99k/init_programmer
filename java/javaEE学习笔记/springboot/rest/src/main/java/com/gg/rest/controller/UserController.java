2c249ae62a8e47e6213cd8fa41f31f9573bded37
GG <gg_2p_2a@163.com>
2020-10-17 15:58:44 +0800

    delUser(...) user(...) updateUser(...)

--- java/javaEE学习笔记/springboot/rest/src/main/java/com/gg/rest/controller/UserController.java
+++ java/javaEE学习笔记/springboot/rest/src/main/java/com/gg/rest/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.gg.rest.controller;
 
 import com.gg.rest.domain.User;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
@@ -19,8 +16,29 @@ public class UserController {
         result = new ArrayList<>(userMap.values());
         return result;
     }
+
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String addUser(@ModelAttribute User user) {
+        String result = "suc";
+        userMap.put(user.getId(), user);
+        return result;
+    }
+
+    @RequestMapping(value = "/{deleteId}", method = RequestMethod.DELETE)
+    public String delUser(@PathVariable int deleteId) {
+        String result = "suc";
+        userMap.remove(deleteId);
+        return result;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User user(@PathVariable int id) {
+        User result = userMap.get(id);
+        return result;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public String updateUser(@ModelAttribute User user) {
         String result = "suc";
         userMap.put(user.getId(), user);
         return result;
--- java/javaEE学习笔记/springboot/rest/src/test/java/com/gg/rest/controller/TestUserController.java
+++ java/javaEE学习笔记/springboot/rest/src/test/java/com/gg/rest/controller/TestUserController.java
@@ -49,4 +49,58 @@ public class TestUserController {
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"testName\"}]")));
     }
+
+    @Test
+    public void deleteUser() throws Exception {
+        requestBuilder = post("/user/")
+                .param("id", "1")
+                .param("name", "testName1");
+        mvc.perform(requestBuilder);
+        requestBuilder = post("/user/")
+                .param("id", "2")
+                .param("name", "testName2");
+        mvc.perform(requestBuilder);
+
+        requestBuilder = delete("/user/1");
+        mvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().string(equalTo("suc")));
+
+        requestBuilder = get("/user/");
+        mvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().string(equalTo("[{\"id\":2,\"name\":\"testName2\"}]")));
+
+
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+        requestBuilder = post("/user/")
+                .param("id", "1")
+                .param("name", "testName1");
+        mvc.perform(requestBuilder);
+        requestBuilder = get("/user/1");
+        mvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().string(equalTo("{\"id\":1,\"name\":\"testName1\"}")));
+    }
+
+    @Test
+    public void TestUpdateUser() throws Exception {
+        requestBuilder = post("/user/")
+                .param("id", "1")
+                .param("name", "testName1");
+        mvc.perform(requestBuilder);
+        requestBuilder = put("/user/")
+                .param("id", "1")
+                .param("name", "changedName");
+        mvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().string(equalTo("suc")));
+        requestBuilder = get("/user/1");
+        mvc.perform(requestBuilder)
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
